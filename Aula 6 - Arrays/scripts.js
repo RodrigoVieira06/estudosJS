64ac1da6526ea18e3e6542ddcc199239cf10e47f
RodrigoVieira06 <rodrigovlima06@gmail.com>
2021-12-19 03:21:49 -0300

    Aula 6 - Treinamento JS - Arrays

---
+++ Aula 6 - Arrays/scripts.js
@@ -0,0 +1,7 @@
+//Arrays são um conjunto de dados que podem ser acessado por um índice.<br />
+//Índice é uma posição exata de onde se encontra uma informação (dado) nesse conjunto de informações.<br /><br />
+
+let familia = [true,45,'Rodrigo',17];
+
+console.log(familia.length);
+console.log(familia[0]); 
