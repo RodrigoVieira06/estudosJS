348349362233875d8fc688e25d97d14ec2306736
RodrigoVieira06 <rodrigovlima06@gmail.com>
2021-12-18 23:21:19 -0300

    Aula 2 - Treinamento JS - Contantes + correções

---
+++ Aula1 - Variaveis/scripts.js
@@ -0,0 +1,9 @@
+let idade = 5;
+let altura = 163;
+console.log(idade + " anos")
+console.log(altura + " cm")
+
+let a,b,c;
+a = b = c = "3"
+
+console.log(a+b+c)
---
+++ Aula2 - Constantes/scripts.js
@@ -0,0 +1,6 @@
+const valorInfressoAdulto = 20;
+
+console.log(valorInfressoAdulto)
+
+//Const deve ser usado para dados que não podem ser alterados
+//é uma boa prática que deve ser utilizada
