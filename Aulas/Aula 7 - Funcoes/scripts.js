2495bb00c42c3ffbffbc15c8266c7517433ce479
RodrigoVieira06 <rodrigovlima06@gmail.com>
2021-12-19 16:08:16 -0300

    Aula 7 - Treinamento JS - Funções

---
+++ Aulas/Aula 7 - Funcoes/scripts.js
@@ -0,0 +1,9 @@
+let corSite = "vermelho";
+
+function resetaCor(cor,tonalidade){
+    corSite = cor + " " +tonalidade;
+};
+
+console.log(corSite);
+resetaCor("preto","claro");
+console.log(corSite);
