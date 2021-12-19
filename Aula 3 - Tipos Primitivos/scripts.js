e698ad01ba9e0ca63797cd00f1303dc080e5e00f
RodrigoVieira06 <rodrigovlima06@gmail.com>
2021-12-19 02:55:45 -0300

    Aula 3 - Treinamento JS - Tipos Primitivos + ajustes nos HTMLs

---
+++ Aula 3 - Tipos Primitivos/scripts.js
@@ -0,0 +1,5 @@
+let nome = "Rodrigo"; //string literal
+let idade = 26; //number literal
+let estaAprovado = true; //boolean
+let sobrenome; //Undefined ou atribuir a 'undefined'
+let corSelecionado = null; //Redefinir um valor
