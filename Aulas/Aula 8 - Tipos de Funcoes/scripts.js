45fa1be7cc227a18d97fd0694b588e98d85c92f0
RodrigoVieira06 <rodrigovlima06@gmail.com>
2021-12-19 16:19:56 -0300

    Aula 8 - Treinamento JS - Tipos de Funções

---
+++ Aulas/Aula 8 - Tipos de Funcoes/scripts.js
@@ -0,0 +1,15 @@
+//Realiza uma tarefa, mas não devolve(retorna) algum dado.
+function dizerNome(){
+    console.log('Rodrigo');
+}
+dizerNome();
+
+//Faz um cálculo ou operação e retorna algum dado.
+function MultiplicarPorDois(valor){
+
+    return valor * 2;
+}
+let resultado = MultiplicarPorDois(5);
+console.log(resultado);
+
+console.log(MultiplicarPorDois(2));
