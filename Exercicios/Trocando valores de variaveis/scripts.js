439b6cd39cb61f95d34c4512dc8e6dca5669988e
RodrigoVieira06 <rodrigovlima06@gmail.com>
2021-12-21 23:51:16 -0300

    Exercício 1 - Treinamento JS - Trocando Valores de Variáveis

---
+++ Exercicios/Trocando valores de variaveis/scripts.js
@@ -0,0 +1,13 @@
+function trocarValores(valor1, valor2) {
+    console.log("Valor 1 =", valor1)
+    console.log("Valor 2 =", valor2)
+
+    console.log("Operando troca...")
+
+    let valor3 = valor1;
+    valor1 = valor2;
+    valor2 = valor3;
+
+    console.log("Valor 1 =", valor1)
+    console.log("Valor 2 =", valor2)
+}
